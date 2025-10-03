3ae61174b518280066241a6cffcc5834be59e2b2
Kien <hoangquanlatoi@gmail.com>
2025-10-03 15:37:19 +0700

    UART and CAN driver run successfull

--- F405_CAN/CSP/include/csp/arch/csp_time.h
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-
-#include <inttypes.h>
-
-uint32_t csp_get_ms(void);
-uint32_t csp_get_ms_isr(void);
-uint32_t csp_get_s(void);
-uint32_t csp_get_s_isr(void);
--- F405_CAN/Debug/CSP/src/csp_debug.d
+++
@@ -1,3 +0,0 @@
-CSP/src/csp_debug.o: ../CSP/src/csp_debug.c \
- D:/TASK_Y4/F405_CAN/CSP/csp_autoconfig.h
-D:/TASK_Y4/F405_CAN/CSP/csp_autoconfig.h:
